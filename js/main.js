902107b89676b4bf461155aa274ae8a7123e3bf2
ahmad <83764912+AhmadHelal6068@users.noreply.github.com>
2022-07-02 06:51:00 -0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// window.addEventListener("load", () => {
-//     document.querySelector(".page-loader").classList.add("slide-out-right");
-//     setTimeout(() => {
-//         document.querySelector(".page-loader").style.display = "none";
-//     }, 1000);
-// });
+window.addEventListener("load", () => {
+    document.querySelector(".page-loader").classList.add("slide-out-right");
+    setTimeout(() => {
+        document.querySelector(".page-loader").style.display = "none";
+    }, 1000);
+});
 
 
 
