60f1cde2f7b2652da6aee5e0ee68396533f33744
ahmad <83764912+AhmadHelal6068@users.noreply.github.com>
2022-07-05 11:57:23 -0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -292,9 +292,9 @@ portfolioBtn.forEach(btn => {
 
 
 
-window.addEventListener("load", () => {
-    document.querySelector(".page-loader").classList.add("slide-out-right");
-    setTimeout(() => {
-        document.querySelector(".page-loader").style.display = "none";
-    }, 1000);
-});
+// window.addEventListener("load", () => {
+//     document.querySelector(".page-loader").classList.add("slide-out-right");
+//     setTimeout(() => {
+//         document.querySelector(".page-loader").style.display = "none";
+//     }, 1000);
+// });
